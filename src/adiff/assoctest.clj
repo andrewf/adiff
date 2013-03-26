4b0447a24d55653246fb0eb93d261c51daa427cd
Andrew Fleenor <andrewflnr@gmail.com>
2013-03-26 15:14:03 -0700

    on assoctest, some real testing

--- src/adiff/assoctest.clj
+++ src/adiff/assoctest.clj
@@ -6,16 +6,12 @@
   (:import java.io.FileReader)
   (:import java.io.PushbackReader))
 
-(def testdata
-  [
-    [[] [] []]
-  ])
 
 (defn readfile
   "java.io.File -> clojure data structures, all of them"
   [f]
   (let [pbr (PushbackReader. (FileReader. f))
-         eof (Object.) ]
+        eof (Object.) ]
     (take-while #(not= % eof) (repeatedly #(read pbr false eof)))))
 
 
@@ -28,13 +24,49 @@
       (println "fail")
       (println "pass"))))
 
+(defn sugarify
+  "make sure that %D, %I, (% ...) do the right thing"
+  [patches]
+  (let [mapper (fn [item]
+                 (cond
+                   (coll? item) (sugarify item)
+                   (= item '%D) %D
+                   (= item '%I) %I
+                   :else item))]
+    (map mapper patches)))
+ 
+(defn test-three
+  [a b result]
+  (if-let [found (try
+                 (compose a b)
+                 (catch UnsupportedOperationException e
+                   (println (str "failed to compose: " e))
+                   nil))]
+    (if (not= found result)
+      (println (str found " != " result))
+      (println "success"))))
+
+(defn test-four
+  [a b c result]
+  (println "haha, just kidding"))
+
+(defn testfile
+  "check the first two or three items, composed, against the last item in the file"
+  [f]
+  (let [[a b c d] (sugarify (readfile f))]
+    ; if d is nil, no assoc test, just a*b=c
+    (if (= d nil)
+      (test-three a b c)
+      (test-four a b c d))))
+
 (defn -main [dirname]
-  (println "hello world")
   (doseq [f (.listFiles (File. dirname))]
-    (println (str "reading " (.getName f)))
-    (println (readfile f))
-  )
-)
+    (let [fname (.getName f)]
+      (if (.endsWith fname ".edn")
+        (do
+          (println (str "reading " (.getName f)))
+          (testfile f))))
+  ))
 
   ;(testthem [%D :y (% [%I %D :x %I]) %I]
   ;          [%I (% [:a :b %I %D]) (% [%I %I 3])]
