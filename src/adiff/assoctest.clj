e51bcb6b764ca29e4e5186d2e00005be2dd37acf
Andrew Fleenor <andrewflnr@gmail.com>
2013-03-26 18:24:08 -0700

    add test-four

--- src/adiff/assoctest.clj
+++ src/adiff/assoctest.clj
@@ -35,20 +35,44 @@
                    :else item))]
     (map mapper patches)))
  
+(defn maybe-compose
+  "either compose them and return [composed, nil] or, if there's an error, return [nil, exception]"
+  [a b]
+  (try
+    [(compose a b) nil]
+    (catch UnsupportedOperationException e
+      [nil e])))
+  
+
 (defn test-three
   [a b result]
-  (if-let [found (try
-                 (compose a b)
-                 (catch UnsupportedOperationException e
-                   (println (str "failed to compose: " e))
-                   nil))]
-    (if (not= found result)
-      (println (str found " != " result))
-      (println "success"))))
+  (let [[found  e] (maybe-compose a b)]
+    (if (not= found nil)
+      (if (not= found result)
+        (println (str found " != " result))
+        (println "success"))
+      (println (str "failed to do compose: " e)))))
 
 (defn test-four
   [a b c result]
-  (println "haha, just kidding"))
+  (let [[ab ab-err] (maybe-compose a b)
+        [bc bc-err] (maybe-compose b c)]
+    (if (not (nil? ab))
+      (if (not (nil? bc))
+        ; try the next layer of compositions
+        (let [[ab-c ab-c-err] (maybe-compose ab c)
+              [a-bc a-bc-err] (maybe-compose a bc)]
+          (if (not (nil? ab-c))
+            (if (not (nil? a-bc))
+              (do
+                (if (not= ab-c result)
+                  (println (str "left-association is wrong " ab-c " != " result)))
+                (if (not= a-bc result)
+                  (println (str "right-association is wrong " a-bc " != " result))))
+              (println (str "right-association failed: " a-bc-err)))
+            (println (str "left-association failed: " ab-c-err))))
+        (println (str "right composition failed: " bc-err)))
+      (println (str "left composition failed: " ab-err)))))
 
 (defn testfile
   "check the first two or three items, composed, against the last item in the file"
