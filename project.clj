daeb7b991dc177d07d52b7a4d3eac4bf2cb2c375
Andrew Fleenor <andrewflnr@gmail.com>
2013-05-04 19:30:25 -0700

    use clojure.edn to parse test files, requires clojure 1.5.0

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "MIT license"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
